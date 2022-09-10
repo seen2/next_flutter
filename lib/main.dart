6563ded9fb2dcb1a57a951ccb5d91a03179dcbf8
seen2 <tiwarietc@gmail.com>
2022-09-10 22:54:44 +0530

    Code modularization

--- lib/main.dart
+++ lib/main.dart
@@ -10,48 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateTitle: (BuildContext context) => "My App",
-      home: Scaffold(
-        body: Container(
-            height: double.maxFinite,
-            width: double.maxFinite,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                MyContainer(),
-                MyContainer(),
-                MyContainer(),
-                MyContainer(),
-              ],
-            )),
-      ),
-    );
-  }
-}
-
-class MyContainer extends StatelessWidget {
-  const MyContainer({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Container(
-        height: 100,
-        width: double.maxFinite,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Colors.teal,
-        ),
-        child: const Center(
-            child: Text(
-          "Tap",
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        )),
-      ),
-    );
+    return const MyHomePage();
   }
 }
