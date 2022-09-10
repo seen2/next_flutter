423c6473e0584609498c67fed97cd92759ffb34e
seen2 <tiwarietc@gmail.com>
2022-09-10 22:48:40 +0530

    Starting for GetX

---
+++ lib/controller/tap_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/state_manager.dart';
+
+class TapController extends GetxController {
+  int _x = 0;
+
+  int get x => _x;
+
+  void increaseX() {
+    _x++;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:next_flutter/my_home_%20page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,16 +12,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      onGenerateTitle: (BuildContext context) => "My App",
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("App Title"),
-
-        ),
-        body: const Center(child:  Text("Hello World")),
-        
+        body: Container(
+            height: double.maxFinite,
+            width: double.maxFinite,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                MyContainer(),
+                MyContainer(),
+                MyContainer(),
+                MyContainer(),
+              ],
+            )),
       ),
-
     );
+  }
+}
 
+class MyContainer extends StatelessWidget {
+  const MyContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        height: 100,
+        width: double.maxFinite,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.teal,
+        ),
+        child: const Center(
+            child: Text(
+          "Tap",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        )),
+      ),
+    );
   }
 }
